8bc282f313b29ac842061260d42ad9775e6ded61
DEMET DÜZ <40537045+Dedos22@users.noreply.github.com>
2023-05-20 20:10:14 +0300

    Update kdvtutarhesap.java

--- kdvtutarhesap.java
+++ kdvtutarhesap.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
  
-public class Main{
+public class kdvtutarhesap{
      public static void main(String[] args) {
        Scanner inp=new Scanner(System.in);
        System.out.print("fiyat giriniz");
