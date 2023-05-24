80cb9fbfabaa21ca6df1ce8e267fa52b29900644
DEMET DÜZ <40537045+Dedos22@users.noreply.github.com>
2023-05-24 20:52:31 +0300

    Update HesapMakinesi.java

--- HesapMakinesi.java
+++ HesapMakinesi.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class HesapMakinesi{
-   public static void main[String[] args]{
+   public static void main(String[] args){
        int a,b,select;
        Scanner input=new Scanner(System.in);
        System.out.print("birinci sayı:");
