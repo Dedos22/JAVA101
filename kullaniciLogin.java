1e511061bf732f251b7b278a56fc2ef9f2a8ba71
DEMET DÜZ <40537045+Dedos22@users.noreply.github.com>
2023-05-24 10:24:00 +0300

    Create kullaniciLogin.java

---
+++ kullaniciLogin.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class kullaniciLogin{
+  public static void main(String[] args){
+     String password="abc123",name="java";
+     String ad,sifre;
+     Scanner input=new Scanner(System.in);
+     System.out.print("Kullanıcı adınızı  giriniz:");
+     ad=input.nextLine();
+     System.out.print("Şifrenizi  giriniz:");
+     sifre=input.nextLine();
+    if(name==ad && sifre==password){
+      System.out.println("login success ");
+    }else{
+      System.out.println("Yanlış şifre ,Şifrenizi sıfırlamak ister misiniz?1-Evet 2-Hayır");
+      int select;
+      select=input.nextInt();
+      if(select==1){
+         System.out.println("Yeni sifre giriniz:");
+         String yenisifre=input.nextLine();
+         password=yenisifre;
+         System.out.println("şifreniz sıfırlandı");
+      }else{
+           System.out.println("şifreniz sıfırlanmadı.İyi gunler");
+      }
+      
+    }
+    
+  }
+}
