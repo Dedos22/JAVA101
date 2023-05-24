87440a6bf84cbafb287728569b0edcf05097bf29
DEMET DÜZ <40537045+Dedos22@users.noreply.github.com>
2023-05-24 20:42:36 +0300

    Update kdvtutarhesap.java

--- kdvtutarhesap.java
+++ kdvtutarhesap.java
@@ -4,21 +4,25 @@ public class kdvtutarhesap{
      public static void main(String[] args) {
        Scanner inp=new Scanner(System.in);
        System.out.print("fiyat giriniz");
-       double fiyat=inp.nextDouble();
-       int  kdvoran=18;
-       double kdvFiyat, kdvtutar;
+      double fiyat=inp.nextInt();
+      double  kdvoran=18;
+       double kFiyat, ktutar;
        if(0<fiyat && fiyat<=1000){
-          kdvtutar=fiyat*(kdvoran/100);
-          kdvFiyat=kdvtutar+fiyat;
+          ktutar=fiyat*kdvoran/100;
+          kFiyat=ktutar+fiyat;
+           System.out.println("KDV'siz Fiyat:"+ fiyat); 
+       System.out.println("KDV'li Fiyat:"+ kFiyat);
+       System.out.println("KDV tutarı:"+ ktutar);
        }else if(fiyat>1000){
            kdvoran=8;
-           kdvtutar=fiyat*(kdvoran/100);
-           kdvFiyat=kdvtutar+fiyat;
+           ktutar=fiyat*kdvoran/100;
+           kFiyat=ktutar+fiyat;
+            System.out.println("KDV'siz Fiyat:"+ fiyat); 
+       System.out.println("KDV'li Fiyat:"+ kFiyat);
+       System.out.println("KDV tutarı:"+ ktutar);
         }else{
            System.out.print("error");
         }
-       System.out.println("KDV'siz Fiyat:"+ fiyat); 
-       System.out.println("KDV'li Fiyat:"+ kdvFiyat);
-       System.out.println("KDV tutarı:"+ kdvtutar);
+      
      }
 }
